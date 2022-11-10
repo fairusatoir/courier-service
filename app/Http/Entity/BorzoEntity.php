f0ee925a31e2238f92308d75203800d5665e12e1
IDCB.Fairus Zuhair Azizy Atoir <IDCB.FAIRUSZ@xl.co.id>
2022-11-10 16:18:16 +0700

    fix: mapping attribute

--- app/Http/Entity/BorzoEntity.php
+++ app/Http/Entity/BorzoEntity.php
@@ -19,13 +19,13 @@ class BorzoEntity
         if($raw->is_successful == 'true'){
             $data = [
                 'is_successful' => $raw->is_successful ?? "",
-                'type' => $raw->orders->type ?? "",
-                'order_id' => $raw->orders->order_id ?? "",
-                'vehicle_type_id' => $raw->orders->vehicle_type_id ?? 0,
-                'created_datetime' => $raw->orders->created_datetime ?? "",
-                'finish_datetime' => $raw->orders->finish_datetime ?? "",
-                'matter ' => $raw->orders->matter  ?? "",
-                'total_weight_kg ' => $raw->orders->total_weight_kg  ?? 0,
+                'type' => $raw->order->type ?? "",
+                'order_id' => $raw->order->order_id ?? "",
+                'vehicle_type_id' => $raw->order->vehicle_type_id ?? 0,
+                'created_datetime' => $raw->order->created_datetime ?? "",
+                'finish_datetime' => $raw->order->finish_datetime ?? "",
+                'matter ' => $raw->order->matter  ?? "",
+                'total_weight_kg ' => $raw->order->total_weight_kg  ?? 0,
             ];
         }else{
             $data = $raw;
