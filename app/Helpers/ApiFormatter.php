0a56917142a9e8ca3e56741d88ce72bfa45a2ebc
Fairus Atoir <36189723+fairusatoir@users.noreply.github.com>
2022-11-11 15:58:54 +0700

    fix: api formatter concept

--- app/Helpers/ApiFormatter.php
+++ app/Helpers/ApiFormatter.php
@@ -4,11 +4,6 @@ namespace App\Helpers;
 
 class ApiFormatter
 {
-    protected static $response =  [
-        'code' =>  null,
-        'message' => null,
-        'data' => null,
-    ];
 
     public static function ok($idRequest=null, $message = null, $data = null)
     {
@@ -31,11 +26,13 @@ class ApiFormatter
             "X-Request-Id"=>$idRequest
         ];
 
-        self::$response['code'] = $code;
-        self::$response['message'] = $message;
-        self::$response['data'] = $data;
+        $body = [
+            "code" => $code,
+            "message" => $message,
+            "data" => $data,
+        ];
 
         return response()
-            ->json(self::$response, self::$response['code'], $header);
+            ->json($body, $code, $header);
     }
 }
