67c2918f34d79e1b25d87f9d83f7c5840ac365fc
Fairus Atoir <36189723+fairusatoir@users.noreply.github.com>
2022-11-12 12:37:23 +0700

    add: payment

---
+++ database/migrations/2022_11_12_052526_create_payment_courier_types_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_courier_types', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('vendor_id');
+            $table->foreign('vendor_id')->references('id')->on('vendors');
+            $table->string('payment_type_params');
+            $table->string('payment_type_name');
+            $table->text('description');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_courier_types');
+    }
+};
