3b2327dc784a37226c75b194cf1c4c7a871715c1
Fairus Atoir <36189723+fairusatoir@users.noreply.github.com>
2022-11-12 09:24:22 +0700

    add: response order price calculation

--- app/Http/Entity/BorzoEntity.php
+++ app/Http/Entity/BorzoEntity.php
@@ -24,8 +24,25 @@ class BorzoEntity
                 'vehicle_type_id' => $raw->order->vehicle_type_id ?? 0,
                 'created_datetime' => $raw->order->created_datetime ?? "",
                 'finish_datetime' => $raw->order->finish_datetime ?? "",
-                'matter ' => $raw->order->matter  ?? "",
-                'total_weight_kg ' => $raw->order->total_weight_kg  ?? 0,
+                'status' => $raw->order->status ?? "",
+                'status_description' => $raw->order->status_description ?? "",
+                'matter' => $raw->order->matter  ?? "",
+                'total_weight_kg' => $raw->order->total_weight_kg  ?? 0,
+                'payment_amount' => $raw->order->payment_amount  ?? 0,
+                'payment_amount_detail' => [
+                    'delivery_fee_amount' => $raw->order->delivery_fee_amount  ?? 0,
+                    'weight_fee_amount' => $raw->order->weight_fee_amount  ?? 0,
+                    'insurance_fee_amount' => $raw->order->insurance_fee_amount  ?? 0,
+                    'loading_fee_amount' => $raw->order->loading_fee_amount  ?? 0,
+                    'money_transfer_fee_amount' => $raw->order->money_transfer_fee_amount  ?? 0,
+                    'overnight_fee_amount' => $raw->order->overnight_fee_amount  ?? 0,
+                    'promo_code_discount_amount' => $raw->order->promo_code_discount_amount  ?? 0,
+                    'cod_fee_amount' => $raw->order->promo_code_discount_amount  ?? 0,
+                    'promo_code_discount_amount' => $raw->order->promo_code_discount_amount  ?? 0
+                ],
+                'backpayment_details' => $raw->order->backpayment_details  ?? "",
+                'is_motobox_required' => $raw->order->backpayment_details  ?? false,
+                'applied_promo_code' => $raw->order->backpayment_details  ?? "",
             ];
         }else{
             $data = $raw;
