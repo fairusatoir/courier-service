bce0e1ffddad678382a15354d8f219bb3b40981d
Fairus Atoir <36189723+fairusatoir@users.noreply.github.com>
2022-11-12 12:38:13 +0700

    add: vehicle type

---
+++ database/migrations/2022_11_12_034049_create_vehicle_courier_types_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicle_courier_types', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('vendor_id');
+            $table->foreign('vendor_id')->references('id')->on('vendors');
+            $table->string('vehicle_type_params');
+            $table->string('vehicle_type_name');
+            $table->text('description');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicle_courier_types');
+    }
+};
